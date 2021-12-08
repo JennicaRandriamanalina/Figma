7469f46d8650aaa62d416dbdba515ba0c5d00914
Jennica Randriamanalina <jennicarandriamanalina@gmail.com>
2021-12-08 11:46:51 +0300

    feat(hom): add contents in the js file

---
+++ Assets/js/main.js
@@ -0,0 +1,27 @@
+function styl0(){
+    document.getElementById("sary").style.visibility="visible";
+    document.getElementById("captures").style.visibility="visible";
+    document.getElementById("captures").style.display="inline";
+    document.getElementById("images").style.visibility="hidden";
+    document.getElementById("images").style.display="none";
+    document.getElementById("tout").style.display="none";
+    document.getElementById("tout").style.visibility="hidden";
+    }
+    function styl1(){
+        document.getElementById("sary").style.visibility="visible";
+        document.getElementById("images").style.visibility="visible";
+        document.getElementById("images").style.display="inline";
+        document.getElementById("captures").style.visibility="hidden";
+        document.getElementById("captures").style.display="none";
+        document.getElementById("tout").style.display="none";
+        document.getElementById("tout").style.visibility="hidden";
+  
+        
+    }
+    function styl(){
+        document.getElementById("sary").style.visibility="visible";
+        document.getElementById("sary").style.display="inline";
+        document.getElementById("tout").style.display="inline";
+        document.getElementById("tout").style.visibility="visible";
+     
+  }
